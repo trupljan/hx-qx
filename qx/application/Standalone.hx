ee413403af64d25fdb97b91af32ff28dc005dc27
Jan Trupl <jan.trupl@gmail.com>
2016-10-10 12:12:26 +0200

    standalone

---
+++ qx/application/Standalone.hx
@@ -0,0 +1,22 @@
+package qx.application;
+
+/**
+ * For a GUI application that looks & feels like native desktop application
+ * (often called "RIA" - Rich Internet Application).
+ *
+ * Such a stand-alone application typically creates and updates all content
+ * dynamically. Often it is called a "single-page application", since the
+ * document itself is never reloaded or changed. Communication with the server
+ * is done with AJAX.
+ *
+ * @require(qx.core.Init)
+ */
+@:native("qx.application.Standalone")
+extern class Standalone
+extends AbstractGui
+{
+
+	/** Create root widget */
+	function _createRootWidget(): qx.ui.root.Application;
+	
+}
---
+++ qx/ui/root/Application.hx
@@ -0,0 +1,12 @@
+package qx.ui.root;
+
+@:native("qx.ui.root.Application")
+class Application
+{
+
+	public function new() 
+	{
+		
+	}
+	
+}
